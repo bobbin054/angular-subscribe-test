027be6cce24a8491e567741f8f0e78ca089a5bf2
bobbin054 <jocken2@hotmail.com>
2022-04-14 11:44:27 +0200

    1 file modified

--- src/app/sub/sub.component.ts
+++ src/app/sub/sub.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from "@angular/core";
-import { from, map, Observable, of } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { from, map, Observable, of, tap } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
-  selector: "sub",
-  templateUrl: "./sub.component.html",
-  styleUrls: ["./sub.component.css"],
+  selector: 'sub',
+  templateUrl: './sub.component.html',
+  styleUrls: ['./sub.component.css'],
 })
 export class SubComponent implements OnInit {
   // Create observable that emits three values
@@ -14,38 +15,50 @@ export class SubComponent implements OnInit {
 
   // Create a observer that logs to the console.
   _myObserver = {
-    next: (x: number) => console.log("Observer got a next value: " + x),
-    error: (err: Error) => console.error("Observer got an error: " + err),
-    complete: () => console.log("Observer got a complete notification"),
+    next: (x: number) => console.log('Observer got a next value: ' + x),
+    error: (err: Error) => console.error('Observer got an error: ' + err),
+    complete: () => console.log('Observer got a complete notification'),
   };
 
   ngOnInit() {
     // Sub to the observable using the observer.
-    this._myObservable.subscribe(this._myObserver);
+    // this._myObservable.subscribe(this._myObserver);
 
     //using subscribe and passing an observer object.
-    of(1, 2, 3).subscribe({
-      next: (x: number) => console.log("Observer got a next value: " + x),
-      error: (err: Error) => console.error("Observer got an error: " + err),
-      complete: () => console.log("Observer got a complete notification"),
-    });
+    // of(1, 2, 3).subscribe({
+    //   next: (x: number) => console.log('Observer got a next value: ' + x),
+    //   error: (err: Error) => console.error('Observer got an error: ' + err),
+    //   complete: () => console.log('Observer got a complete notification'),
+    // });
 
-    of(1, 2, 3).pipe(map((x) => console.log("Yo! ",x)));
+    of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+      .pipe(
+        tap((element) => {
+          console.log('tap =>', element);
+        }),
+        map((element, i) => {
+          element = element / 2;
+          return element * 10;
+        })
+      )
+      .subscribe({
+        next: (x: number) => console.log('sub next => ' + x),
+      });
 
-    from([
-      "hello 1",
-      1,
-      "hello 3",
-      "hello 4",
-      "hello 5",
-      "hello 6",
-      "hello 7",
-      "hello 8",
-      2.3,
-    ]).subscribe({
-      next: (x: string) => console.log("Observer got a next value: " + x),
-      error: (err: Error) => console.error("Observer got an error: " + err),
-      complete: () => console.log("Observer got a complete notification"),
-    });
+    // from([
+    //   'msg 1',
+    //   1,
+    //   'msg 3',
+    //   'msg 4',
+    //   'msg 5',
+    //   'msg 6',
+    //   'msg 7',
+    //   'msg 8',
+    //   2.3,
+    // ]).subscribe({
+    //   next: (x: string) => console.log('Observer got a next value: ' + x),
+    //   error: (err: Error) => console.error('Observer got an error: ' + err),
+    //   complete: () => console.log('Observer got a complete notification'),
+    // });
   }
 }
