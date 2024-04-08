4d3343707978e0a997bc1632bcead25117f10698
bobbin054 <80533145+bobbin054@users.noreply.github.com>
2024-04-08 21:07:11 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,59 +7,37 @@ import { take } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // Create observable that emits three values
-  private _myObservable: Observable<number> = of(1, 2, 3);
-
-  constructor() {}
-
+  myObservable: Observable<number> = of(1, 2, 3);
   // Create a observer that logs to the console.
-  _myObserver = {
+  myObserver = {
     next: (x: number) => console.log('Observer got a next value: ' + x),
     error: (err: Error) => console.error('Observer got an error: ' + err),
     complete: () => console.log('Observer got a complete notification'),
   };
+  tapList: number[] = [];
+  mapList: number[] = [];
 
   ngOnInit() {
-    // Sub to the observable using the observer.
-    // this._myObservable.subscribe(this._myObserver);
-
-    //using subscribe and passing an observer object.
-    // of(1, 2, 3).subscribe({
-    //   next: (x: number) => console.log('Observer got a next value: ' + x),
-    //   error: (err: Error) => console.error('Observer got an error: ' + err),
-    //   complete: () => console.log('Observer got a complete notification'),
-    // });
-
-    of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    this.myObservable
       .pipe(
         tap((element) => {
-          console.log('tap =>', element);
+          console.log('In tap. element: ', element);
+          this.tapList.push(element);
         }),
         map((element, i) => {
-          element = element / 2;
-          return element * 10;
+          console.log('In map. mutating element * 2');
+          const result= element * 2;
+          this.mapList.push(result);
+          return result;
         })
       )
-      .subscribe({
-        next: (x: number) => console.log('sub next => ' + x),
-        complete: () => {
-          console.log('Done');
-        },
-      });
+      .subscribe(this.myObserver);
 
-    // from([
-    //   'msg 1',
-    //   1,
-    //   'msg 3',
-    //   'msg 4',
-    //   'msg 5',
-    //   'msg 6',
-    //   'msg 7',
-    //   'msg 8',
-    //   2.3,
-    // ]).subscribe({
-    //   next: (x: string) => console.log('Observer got a next value: ' + x),
+    //create observerable, subscribe, observer object.
+    // of(1, 2, 3).subscribe({
+    //   next: (x: number) => console.log('Observer got a next value: ' + x),
     //   error: (err: Error) => console.error('Observer got an error: ' + err),
     //   complete: () => console.log('Observer got a complete notification'),
     // });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
